761d8855e8fc7c5ee563ab48087ae48351adcd86
malustewart <mastewart@itba.edu.ar>
2025-09-07 12:37:16 -0300

    main algo + fix get_freq

--- FO.py
+++ FO.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.fft import fft, ifft, fftfreq
 from scipy.signal import find_peaks
 from tqdm.auto import tqdm
+import matplotlib.pyplot as plt
 
 def get_frequency(A, fs):
     a = np.abs(fft(A))
@@ -13,8 +14,8 @@ def get_frequency(A, fs):
 
     prom = 0.5 * np.max(a)
     peaks = find_peaks(a, prominence=prom)  # prominence evita detectar falsos picos por ruido en el espectro
-    
-    return f[peaks[0]]
+
+    return f[peaks[0][0]]
 
 def optical_fiber(
     A: np.ndarray,          # Señal óptica de entrada (array complejo, shape: (n_samples,))
@@ -61,56 +62,74 @@ def optical_fiber(
     alpha_np = alpha * np.log(10) / 10
     
     # Paso 2: Calcular frecuencias angulares ω (en rad/ps)
-    w = get_frequency(A, fs)
-    
+    w = get_frequency(A, fs) * 2 * np.pi
+
     # Paso 3: Operador lineal D en dominio de frecuencia
-    D_op = ...
+    D_op = -alpha_np/2 + beta_2*w*w/2*1j + beta_3*w*w*w/6*1j
     
     # Paso 4: Inicializar paso adaptativo h
     if gamma == 0 or (beta_2 == 0 and beta_3 == 0):
         h = length  # Caso sin dispersión o no linealidad, la solución es exacta
     else:
-        h = ...  # Calcular paso inicial basado en P_max y phi_max
+        P_max = np.max(np.abs(A*A))
+        h = phi_max/gamma/P_max
     
     h = min(h, length)  # Asegurar que h no exceda la longitud total
 
     # Inicializar posición actual
-    z = 0.0
     steps = 0
+    z = 0.0
+    prev_z = 0.0
 
     # ignorar esta linea..
     progress_bar = tqdm(total=100, desc="Progreso de propagación", bar_format="{l_bar}{bar}|[{elapsed}{postfix}]", postfix={"steps": 0})
     
     # Paso 5: Bucle principal de SSFM adaptativo
     while z < length:
-        z += h # Avanzar posición
-        step += 1 # Actualizar número de pasos
+        z = prev_z + h
+
+        A_squared = abs(A*A)
+
         progress_bar.set_postfix({"steps": steps, "z": z}) # ignorar esta linea..
 
-
         ## Implementar SSFM simétrico
-        N_op = ...
+        N_op = gamma * (np.abs(A_squared)) * 1j
+        
         # Paso 5.1: Operador N en h/2
-        ...
+        A = np.exp(N_op*h/2)*A
 
         # Paso 5.2: Operados D en h
-        ...
+        A = ifft(np.exp(D_op*h)*fft(A))
 
         # Paso 5.3: Operador N en h/2
-        ...
+        A = np.exp(N_op*h/2)*A
 
+        progress_bar.update(100 * z / length) # ignorar esta linea..
 
-        progress_bar.update(100 * h / length) # ignorar esta linea..
+        prev_z = z
+        
+        # Paso 5.4: Si se optó por la implemntación adaptiva, calcular nueva h
+        if gamma == 0 or (beta_2 == 0 and beta_3 == 0):
+            pass
+        else:
+            P_max = np.max(A_squared)
+            h = phi_max/gamma/P_max
+            h = min(h, length - z)  # Asegurar que h no exceda la longitud restante
 
-        # Paso 5.4: Si se optó por la impleemntación adaptiva, calcular nueva h, sino eliminar estas lineas
-        h = ...
-        h = min(h, length - z)  # Asegurar que h no exceda la longitud restante
-    
+        z += h # Avanzar posición
+        steps += 1 # Actualizar número de pasos
+
     progress_bar.close() # ignorar esta linea..
     
     return A
 
 
-fs = 1000
+fs = 6000
 t = np.linspace(0, 1, fs, endpoint=False)
-x = np.exp(2j*np.pi*123*t) + 0.5*np.exp(2j*np.pi*300*t)
+A = np.exp(2j*np.pi*123*t) + 0.5*np.exp(2j*np.pi*300*t)
+
+B = optical_fiber(A, fs, 20, 0, 0, 0, 0, 0)
+
+plt.plot(t, A)
+plt.plot(t, B)
+plt.show()
