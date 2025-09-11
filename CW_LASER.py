5d467e37eaf485485f36ae303f07fabfbebba7ab
malustewart <mastewart@itba.edu.ar>
2025-09-10 22:21:43 -0300

    minor fixes, updates on generating labels

--- CW_LASER.py
+++ CW_LASER.py
@@ -8,9 +8,9 @@ from dataclasses import dataclass
 class CWLaserParams:
     t: np.ndarray
     P0: float
-    lw: float = 0.0
+    lw: float = 0.0 # Hz
     rin: float = -np.inf
-    df: float = 0.0
+    df: float = 0.0 # Hz
 
 def cw_laser(params: CWLaserParams) -> np.ndarray:
     r"""
@@ -34,7 +34,6 @@ def cw_laser(params: CWLaserParams) -> np.ndarray:
 
     dt = t[1] - t[0]    #ps
     fs = 1/dt  # THz
-    print(fs)
 
     n_samples = len(t)
 
@@ -52,19 +51,28 @@ def cw_laser(params: CWLaserParams) -> np.ndarray:
     return Eout
 
 fs = 10  # THz
-length = 1000 # ps
+length = 10000 # ps
 t = np.linspace(0, length, int(length*fs))
 
 params_list = [
-    CWLaserParams(t=t, P0=1, rin=-np.inf, df=0, lw=0),
-    CWLaserParams(t=t, P0=1, rin=-np.inf, df=0, lw=100),
-    CWLaserParams(t=t, P0=1, rin=-np.inf, df=10, lw=0),
+    CWLaserParams(t=t, P0=1, rin=-140, df=0, lw=1e6),
+    CWLaserParams(t=t, P0=1, rin=-140, df=0, lw=1),
+    CWLaserParams(t=t, P0=1, rin=-140, df=0, lw=0.00001),
+    CWLaserParams(t=t, P0=1, rin=-140, df=1, lw=0.00001),
+    # CWLaserParams(t=t, P0=1, rin=-np.inf, df=1000, lw=0),
+    # CWLaserParams(t=t, P0=1, rin=-np.inf, df=1000, lw=1000),
+]
+
+labels = [
+    f"P0={params.P0} dBm, RIN={params.rin} dB/Hz, df={params.df} Hz, lw={params.lw} Hz"
+    for params in params_list
 ]
 
 Eouts = [cw_laser(params) for params in params_list]
 PSDs = [signal.welch(Eout, return_onesided=False, fs=fs) for Eout in Eouts]
 
-plot_signals(Eouts, fs)
-plot_PSD(PSDs)
+
+plot_signals(Eouts, fs, labels)
+plot_PSD(PSDs, labels)
 
 show_plots()
